d66b1bec8791dbd7d5f628bddd8550285f31928d
user <2819815@mail.ru>
2022-10-24 12:53:07 +0300

    Second commi

--- home work 11.py
+++ home work 11.py
@@ -23,54 +23,81 @@ print(«Hello world»), ваша программа должна вывести 
 
 import os
 import shutil
-
+import subprocess, sys
+import random
 
 class Order_on_the_computer():
 
     def work_with_file():
+        ''' Выводит на печать наименование операйионной системы'''
         print(os.name)
 
     def directory_path():
+        """Выводит на печать текущий рабочий католог
+        :return:
+        """
         print(os.getcwd())
 
     def copi_and_sort_file():
+        """ Проверяет сщдержимое католога на принадлежность к файлам, создаeт новые паки
+        по раширению файлов, копируют фалы по пакам в соответвии с их расширеним"""
         data = os.listdir()
         data1 = os.getcwd()
-        # os.mkdir("Sort")
+        new_set = {}
+        new_set_size ={}
+
+
         for i in data:
             if os.path.isfile(i):
+                extenshion = i.split('.')
+                extenshion = str(extenshion[-1::])
+                extenshion = extenshion
+                if os.path.exists(data1+ "/" + extenshion):
+                    shutil.copyfile(data1 + "/" + i, data1 + "/" + extenshion + "/" +i)
 
-                print(data1+"/"+i)
-                shutil.copyfile(data1+"/"+i, data1+"/"+"Sort"+"/"+i)
+                    new_set[extenshion] += 1
+                    new_set_size[extenshion] += os.path.getsize(data1 + "/" + extenshion + "/" +i)
 
+                else:
+                    os.mkdir(data1+ "/" + extenshion)
+                    new_set[extenshion] = 1
+                    new_set_size[extenshion] =0
+                    shutil.copyfile(data1+ "/" +i, data1+ "/" + extenshion + "/" + i)
+                    new_set_size[extenshion] += os.path.getsize(data1 + "/" + extenshion + "/" + i)
 
-
-
-
-
-
-
-
-
-        # b = Order_on_the_computer.directory_path()
-
-
-
-
-
-
+        for i in new_set:
+            print(f"В папку с {i} файлами закинуто {new_set[i]} файлов, их суммарный объем составляет {new_set_size[i]} байт")
 
 
+    def start_file():
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, "World.py"])
+        print(" File World.py is open")
 
+    def rename_file():
+        '''переименовываем случайный файл в новой диектории'''
+        old_file_name = os.listdir(os.getcwd() +'/'+ "['py']")
+        b =random.randint(0, len(old_file_name))
+        old_file_name = old_file_name[b]
 
+        new_file_name = "new" + old_file_name
+        os.rename(old_file_name, new_file_name)
+        print(f"File {old_file_name} rename in  {new_file_name}")
 
 
 
 
 start = Order_on_the_computer
-start.work_with_file()
-start.directory_path()
-start.copi_and_sort_file()
+# start.work_with_file()
+# start.directory_path()
+# start.copi_and_sort_file()
+#start.start_file()
+start.rename_file()
+
+
+
+
+
 
 
 
